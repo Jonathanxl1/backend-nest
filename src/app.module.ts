9428b10f41d1dee2eecaf375b19c4ff27caad51a
Jonathan A <jonathanxl1@hotmail.com>
2025-01-02 14:43:10 -0500

    refactor: Integrate common module to use env file

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,19 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { DbModule } from './db/db.module';
+import { ConfigModule } from '@nestjs/config';
+import config from './config/config';
 
 @Module({
-  imports: [AuthModule, DbModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      isGlobal: true,
+      load: [config],
+    }),
+    AuthModule,
+    DbModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
