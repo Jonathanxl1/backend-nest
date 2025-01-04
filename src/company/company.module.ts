eb4193c39dde793af2d6bede70a1bd891beb9d7d
Jonathan A <jonathanxl1@hotmail.com>
2025-01-04 00:08:45 -0500

    refactor: Added userModule manage data userService

--- src/company/company.module.ts
+++ src/company/company.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { CompanyController } from './company.controller';
 import { CompanyService } from './company.service';
+import { UserModule } from 'src/user/user.module';
 
 @Module({
+  imports: [UserModule],
   controllers: [CompanyController],
-  providers: [CompanyService]
+  providers: [CompanyService],
 })
 export class CompanyModule {}
