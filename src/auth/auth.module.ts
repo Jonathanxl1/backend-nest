fbf62061bb67e8f86e830ba9e78d618f02bfcdfb
Jonathan A <jonathanxl1@hotmail.com>
2025-01-03 19:57:36 -0500

    refactor: Configurate Auth module use Jwt and Guard

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,10 +2,27 @@ import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UserModule } from 'src/user/user.module';
+import { JwtModule } from '@nestjs/jwt';
+import config from 'src/config/config';
+import { ConfigType } from '@nestjs/config';
+import { JwtStrategy } from './strategy/jwt.strategy';
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    JwtModule.registerAsync({
+      inject: [config.KEY],
+      useFactory: (configService: ConfigType<typeof config>) => {
+        return {
+          secret: configService.jwtSecret,
+          signOptions: {
+            expiresIn: '1d',
+          },
+        };
+      },
+    }),
+    UserModule,
+  ],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, JwtStrategy],
 })
 export class AuthModule {}
